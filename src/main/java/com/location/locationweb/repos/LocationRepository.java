010cce46d2bc02f1ab353104cca93791d09030e8
ismael1987 <asmael-1987@hotmail.com>
2018-06-21 11:46:06 +0200

    add the location class

---
+++ src/main/java/com/location/locationweb/repos/LocationRepository.java
@@ -0,0 +1,7 @@
+package com.location.locationweb.repos;
+
+import model.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location,Integer> {
+}
